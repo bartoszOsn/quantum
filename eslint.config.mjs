5d97f2f5f854baac7088c7f11bdfaddb78dc16d4
Bartosz Osiński <osinskibartosz24@gmail.com>
2024-07-01 19:23:44 +0200

    Configure eslint

---
+++ eslint.config.mjs
@@ -0,0 +1,68 @@
+import globals from 'globals';
+import { plugin as TsEslintPlugin, parser as TsEslintParser } from 'typescript-eslint';
+import ImportPlugin from 'eslint-plugin-import';
+import pluginReactConfig from 'eslint-plugin-react/configs/recommended.js';
+import { fixupConfigRules } from '@eslint/compat';
+
+
+export default [
+	{
+		files: ['src/**/*.{js,mjs,cjs,ts,jsx,tsx}']
+	},
+	{ languageOptions: { parserOptions: { ecmaFeatures: { jsx: true } } } },
+	{ languageOptions: { globals: globals.browser } },
+	{
+		files: ['src/**/*.{ts,tsx}'],
+		languageOptions: {
+			parser: TsEslintParser,
+		},
+		plugins: {
+			'@typescript-eslint': TsEslintPlugin,
+			import: ImportPlugin
+		},
+		rules: {
+			"@typescript-eslint/array-type": [
+				"error",
+				{
+					default: 'generic'
+				}
+			],
+			"@typescript-eslint/explicit-function-return-type": [
+				'error',
+				{
+					allowExpressions: true,
+					allowTypedFunctionExpressions: true,
+					allowHigherOrderFunctions: false // ?
+				}
+			],
+			"@typescript-eslint/member-delimiter-style": 'error',
+			"@typescript-eslint/member-ordering": 'error',
+			"@typescript-eslint/no-explicit-any": 'error',
+			"@typescript-eslint/prefer-ts-expect-error": 'warn',
+			"prefer-const": 'error',
+			"no-var": 'error',
+			"no-new-object": 'error',
+			"object-shorthand": ['error', 'always'],
+			"quote-props": ['error', 'consistent-as-needed'],
+			"object-curly-newline": ['error', { 'consistent': true }],
+			"object-curly-spacing": ['error', 'always'],
+			"quotes": ['error', 'single'],
+			"prefer-template": 'warn',
+			"@typescript-eslint/no-loop-func": 'error',
+			"prefer-rest-params": 'error',
+			"@typescript-eslint/space-before-blocks": 'error',
+			"no-param-reassign": 'error',
+			"function-paren-newline": ['error', 'multiline-arguments'],
+			"prefer-arrow-callback": 'error',
+			"import/order": ["error", {"groups": ["builtin", "external", "internal", "parent", "sibling", "index", "object", "type"]}],
+			"@typescript-eslint/no-unused-vars": 'error',
+			'eqeqeq': 'error',
+			'brace-style': 'error',
+			'@typescript-eslint/indent': ['error', 'tab'],
+			'@typescript-eslint/space-infix-ops': 'error',
+			'eol-last': ["error", "always"],
+			'max-len': ['error', {'code': 160}],
+			'no-trailing-spaces': 'error'
+		}
+	}
+];
